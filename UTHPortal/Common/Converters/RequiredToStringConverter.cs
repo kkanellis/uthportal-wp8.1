b966e3a9e4d67180c2521fd7f1aceab3372a9c70
Konstantinos Kanellis <kkanelli@hotmail.com>
2015-08-11 20:03:12 +0300

    CourseView major redesign

---
+++ UTHPortal/Common/Converters/RequiredToStringConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Windows.UI.Xaml.Data;
+
+namespace UTHPortal.Common.Converters
+{
+    class RequiredToStringConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            bool required = (bool)value;
+
+            if (required) {
+                return "Yποχρεωτικό";
+            }
+            else {
+                return "Eπιλογής";
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
